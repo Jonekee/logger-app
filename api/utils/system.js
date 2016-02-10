fb5686aaca9399be93b20256350051342a07ca66
Ross Court <rosscourt93@gmail.com>
2016-02-10 21:32:40 +0000

    Added use of global config file to API server system helper. Also added code to add any essential missing config properties and to write the changes back to disk. Added simple get system settings call for use in the API in a later release.

--- api/utils/system.js
+++ api/utils/system.js
@@ -4,8 +4,6 @@ import { Instance as LoggingManager } from 'logging-manager';
 
 /* eslint func-names:0 */
 
-const relativeSystemFileLocation = global.__LIB_VERSION__ ? '/../../../' : '/../../';
-
 function readFile(file) {
   return new Promise(function(resolve, reject) {
     fs.readFile(file, 'utf-8', function(err, data) {
@@ -15,14 +13,51 @@ function readFile(file) {
   });
 }
 
+function writeFile(file, contents) {
+  return new Promise(function(resolve, reject) {
+    fs.writeFile(file, contents, 'utf-8', function(err) {
+      if (err) reject(err);
+      else resolve();
+    });
+  });
+}
+
 class SystemHelper {
   constructor() {
     LoggingManager.system('SystemHelper', 'constructor', 'Initialising SystemHelper, reading config file...');
-    const systemFile = __dirname + relativeSystemFileLocation + 'system.json';
-    readFile(systemFile)
+    this.fileLocation = global.__CONFIG_FILE__;
+    readFile(this.fileLocation)
       .then((data) => {
         LoggingManager.system('SystemHelper', 'constructor', 'Finished reading system file');
         this.system = data;
+
+        // Check that the essentials are there
+        let configChanged = false;
+
+        if (!this.system.app) {
+          this.system.app = {
+            webport: 8080,
+            apiport: 3030,
+            loglevel: 'ERROR',
+            fakeWebSockets: false
+          };
+          configChanged = true;
+        }
+
+        if (!this.system.users) {
+          this.system.users = {};
+          configChanged = true;
+        }
+
+        if (!this.system.groups) {
+          this.system.groups = [];
+          configChanged = true;
+        }
+
+        if (configChanged) {
+          LoggingManager.info('SystemHelper', 'constructor', 'Config file was missing some properties, defaults are being added.');
+          return this.saveConfigToDisk();
+        }
       })
       .catch((error) => {
         LoggingManager.fatal('SystemHelper', 'constructor', 'Failed to read system file');
@@ -30,6 +65,23 @@ class SystemHelper {
         process.exit(1);
       });
   }
+
+  saveConfigToDisk = () => {
+    LoggingManager.debug('SystemHelper', 'saveConfigToDisk', 'Writing system file to disk');
+    return writeFile(this.fileLocation, JSON.stringify(this.system, null, 2))
+      .then(() => {
+        LoggingManager.debug('SystemHelper', 'saveConfigToDisk', 'Finished writing system file to disk');
+      })
+      .catch((error) => {
+        LoggingManager.fatal('SystemHelper', 'saveConfigToDisk', 'Failed to write file to disk');
+        LoggingManager.fatal('SystemHelper', 'saveConfigToDisk', error);
+        process.exit(1);
+      });
+  };
+
+  getSystemSettings = () => {
+    return this.system.app;
+  };
 
   getGroupNames = () => {
     return Object.keys(this.system.groups);
--- bin/api.js
+++ bin/api.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 var LoggingManager = require('logging-manager').Instance;
 LoggingManager.setLogLevel(process.env.NODE_ENV === 'production' ? 'ERROR' : 'TRACE');
+global.__CONFIG_FILE__ = './system.json';
 
 if (process.env.NODE_ENV !== 'production') {
   if (!require('piping')({
