4d7cdceccee6e9719cb7f9e9c337f56ba5b058b7
Ross Court <rosscourt93@gmail.com>
2016-02-10 21:30:15 +0000

    Added log level override to app startup script. Changed the way that config file is used, reading the default system file if a config parameter is not passed. Also added warnings where hardcoded default values are being used.

--- bin/app.js
+++ bin/app.js
@@ -3,44 +3,91 @@
 
 var LoggingManager = require('logging-manager').Instance;
 var program = require('commander');
+var path = require('path');
 var fs = require('fs');
+
+var defaultWebPort = 8080;
+var defaultAPIPort = 8080;
+var defaultLogLevel = 'ERROR';
 
 program
   .version('0.3.0')
   .option('-p, --port [port]', 'Port to run webserver on.')
   .option('-a, --apiport [apiport]', 'Port to run API server on.')
+  .option('-l, --loglevel [loglevel]', 'Log level to output.')
   .option('-c, --config [config]', 'Config file for app and system.')
   .parse(process.argv);
 
+/*
+This should either use the config file passed, or the system.json already
+installed.
+
+If ports or log level is not found, defaults should be used.
+
+If either config file can't be used then error and exit the process.
+
+If command line argument ports or log level were passed, override config file
+*/
+
 var config;
+var configFile;
 
 if (!!program.config) {
-  LoggingManager.system('APP', 'main', 'Reading config file...');
-  try {
-    config = fs.readFileSync(program.config, { encoding: 'utf8' });
-  } catch (error) {
-    LoggingManager.fatal('APP', 'main', 'Failed to read config file: ' + program.config);
-    LoggingManager.fatal('APP', 'main', error);
-    process.exit(1);
-  }
-  try {
-    config = JSON.parse(config);
-  } catch (error) {
-    LoggingManager.fatal('APP', 'main', 'Config file is not valid JSON: ' + program.config);
-    LoggingManager.fatal('APP', 'main', error);
-    process.exit(1);
-  }
-  LoggingManager.system('APP', 'main', 'Finished reading config file.');
+  LoggingManager.system('APP', 'main', 'Using custom config file');
+  configFile = program.config;
+} else {
+  LoggingManager.system('APP', 'main', 'Using pre-packaged config file');
+  configFile = './system.json';
+}
+
+// Set configFile reference for use by the servers later on
+global.__CONFIG_FILE__ = configFile;
+
+LoggingManager.system('APP', 'main', 'Reading config file: ' + configFile);
+
+// Read file
+try {
+  config = fs.readFileSync(configFile, { encoding: 'utf8' });
+} catch (error) {
+  LoggingManager.fatal('APP', 'main', 'Failed to read config file.');
+  LoggingManager.fatal('APP', 'main', error);
+  process.exit(1);
+}
+
+// Parse JSON
+try {
+  config = JSON.parse(config);
+} catch (error) {
+  LoggingManager.fatal('APP', 'main', 'Config file is not valid JSON.');
+  LoggingManager.fatal('APP', 'main', error);
+  process.exit(1);
+}
+
+// Successfully loaded config
+LoggingManager.system('APP', 'main', 'Finished reading config file.');
+
+
+// Warn user if any default values were used:
+if (!program.port && !(config && config.port)) {
+  LoggingManager.system('APP', 'main', 'Warning! No webserver port value found in config file, using default value: ' + defaultWebPort);
+}
+if (!program.apiport && !(config && config.apiport)) {
+  LoggingManager.system('APP', 'main', 'Warning! No API server port value found in config file, using default value: ' + defaultAPIPort);
+}
+if (!program.loglevel && !(config && config.loglevel)) {
+  LoggingManager.system('APP', 'main', 'Warning! No log level value found in config file, using default value: ' + defaultLogLevel);
 }
 
 
-process.env.PORT = program.port || (config && config.port) || 8080;
-process.env.APIPORT = program.apiport || (config && config.apiport) || 3030;
+// Set values or defaults:
+process.env.PORT = program.port || (config && config.port) || defaultWebPort;
+process.env.APIPORT = program.apiport || (config && config.apiport) || defaultAPIPort;
+LoggingManager.setLogLevel(program.loglevel || (config && config.loglevel) || defaultLogLevel);
 
+
+// App initialisation code:
 LoggingManager.system('APP', 'main', 'Starting app using webserver port: ' + process.env.PORT + ' and API port: ' + process.env.APIPORT);
 
-
-var path = require('path');
 var rootDir = path.resolve(__dirname, '..');
 
 process.env.NODE_ENV = 'production';
