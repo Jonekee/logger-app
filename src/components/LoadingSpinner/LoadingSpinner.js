8550f3c543e78c7a79c9b86434612574805d15a2
Ross Court <rosscourt93@gmail.com>
2016-02-15 23:15:55 +0000

    Added size and strokeWidth as props to LoadingSpinner

--- src/components/LoadingSpinner/LoadingSpinner.js
+++ src/components/LoadingSpinner/LoadingSpinner.js
@@ -1,18 +1,24 @@
-import React, {Component} from 'react';
+import React, { Component, PropTypes } from 'react';
 import styles from './LoadingSpinner.scss';
 
 export default class LoadingSpinner extends Component {
+  static propTypes = {
+    size: PropTypes.number,
+    strokeWidth: PropTypes.number
+  };
+
   componentDidUpdate() {
     console.log('LoadingSpinner:cDU');
   }
 
   render() {
+    const { size, strokeWidth } = this.props;
     return (
-      <article className={styles.loadingSpinner}>
-				<svg>
-					<circle className="path" cx="50" cy="50" r="20" fill="none" strokeWidth="2" stroke-miterlimit="10"/>
-				</svg>
-			</article>
+      <article className={styles.loadingSpinner} style={{ height: size + 'px', width: size + 'px' }}>
+        <svg style={{ height: size + 'px', width: size + 'px' }}>
+          <circle className="path" cx={size / 2} cy={size / 2} r={(size / 2) - (4 * strokeWidth)} fill="none" strokeWidth={strokeWidth || 2} stroke-miterlimit="10"/>
+        </svg>
+      </article>
     );
   }
 }
