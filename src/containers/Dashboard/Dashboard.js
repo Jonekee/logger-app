72e671eb73aa528e36dea86ccf6347c9d966dcd8
Ross Court <rosscourt93@gmail.com>
2016-02-16 22:50:56 +0000

    Added ErrorPanel to top of Dashboard and changed the styling to not overflow the page height

--- src/containers/Dashboard/Dashboard.js
+++ src/containers/Dashboard/Dashboard.js
@@ -1,28 +1,38 @@
 import React, { Component, PropTypes } from 'react';
-import {isLoaded, addLineToLog, load as loadGroups} from '../../redux/modules/groups';
+import {isLoaded as isGroupsLoaded, addLineToLog, load as loadGroups} from '../../redux/modules/groups';
+import {isLoaded as isSystemSettingsLoaded, load as loadSystemSettings, clearError as clearAppSettingsError} from '../../redux/modules/system';
 import connectData from '../../helpers/connectData';
 import { connect } from 'react-redux';
-import { NavPanel } from '../../components';
+import { NavPanel, ErrorPanel } from '../../components';
+
 
 function fetchData(getState, dispatch) {
-  if (!isLoaded(getState())) {
-    return dispatch(loadGroups());
+  const promises = [];
+  if (!isGroupsLoaded(getState())) {
+    promises.push(dispatch(loadGroups()));
   }
+  if (!isSystemSettingsLoaded(getState())) {
+    promises.push(dispatch(loadSystemSettings()));
+  }
+  return Promise.all(promises);
 }
 
 @connectData(fetchData)
 @connect(
   state => ({
     authEnabled: state.auth.enabled,
-    groups: state.groups.data
+    groups: state.groups.data,
+    appSettingsError: state.system.error
   }),
-  { addLineToLog })
+  { addLineToLog, clearAppSettingsError })
 export default class Dashboard extends Component {
   static propTypes = {
     children: PropTypes.object.isRequired,
     authEnabled: PropTypes.bool,
     addLineToLog: PropTypes.func,
-    groups: PropTypes.array
+    groups: PropTypes.array.isRequired,
+    appSettingsError: PropTypes.array.isRequired,
+    clearAppSettingsError: PropTypes.func.isRequired
   };
 
   componentDidMount() {
@@ -32,12 +42,13 @@ export default class Dashboard extends Component {
   }
 
   render() {
-    const { authEnabled, groups } = this.props;
+    const { authEnabled, groups, appSettingsError, clearAppSettingsError } = this.props; // eslint-disable-line
     const styles = require('./Dashboard.scss');
     return (
       <div className={styles.dashboard}>
         <NavPanel authEnabled={authEnabled} groups={groups}/>
         <main>
+          <ErrorPanel appSettingsError={appSettingsError} clearAppSettingsError={clearAppSettingsError}/>
           {this.props.children}
         </main>
       </div>
