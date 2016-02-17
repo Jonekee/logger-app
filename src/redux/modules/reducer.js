c16d9d1c7d01c6fb0680f2980581cef648912ecf
Ross Court <rosscourt93@gmail.com>
2016-02-17 18:46:13 +0000

    Removing all trace of bootstrap and font-awesome. Also removing multi reducer dependancy and the unused redux reducer that was using it

--- src/redux/modules/reducer.js
+++ src/redux/modules/reducer.js
@@ -1,9 +1,7 @@
 import { combineReducers } from 'redux';
-import multireducer from 'multireducer';
 import { routerStateReducer } from 'redux-router';
 
 import auth from './auth';
-import counter from './counter';
 import {reducer as form} from 'redux-form';
 import info from './info';
 import widgets from './widgets';
@@ -14,11 +12,6 @@ export default combineReducers({
   router: routerStateReducer,
   auth,
   form,
-  multireducer: multireducer({
-    counter1: counter,
-    counter2: counter,
-    counter3: counter
-  }),
   info,
   widgets,
   groups,
