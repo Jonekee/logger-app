0c44ef3b51eaa3e78e5ebc6271aeae7f4c277323
Ross Court <rosscourt93@gmail.com>
2016-03-12 00:59:35 +0000

    Change SVG attributes in LoadingSpinner for React 15

--- src/components/LoadingSpinner/LoadingSpinner.js
+++ src/components/LoadingSpinner/LoadingSpinner.js
@@ -1,6 +1,9 @@
 import React, { Component, PropTypes } from 'react';
 import styles from './LoadingSpinner.scss';
 
+// TODO ## Remove this temporary disable
+/* eslint react/no-unknown-property:0 */
+
 export default class LoadingSpinner extends Component {
   static propTypes = {
     size: PropTypes.number,
@@ -16,7 +19,7 @@ export default class LoadingSpinner extends Component {
     return (
       <article className={styles.loadingSpinner} style={{ height: size + 'px', width: size + 'px' }}>
         <svg style={{ height: size + 'px', width: size + 'px' }}>
-          <circle className="path" cx={size / 2} cy={size / 2} r={(size / 2) - (4 * strokeWidth)} fill="none" strokeWidth={strokeWidth || 2} stroke-miterlimit="10"/>
+          <circle class="path" cx={size / 2} cy={size / 2} r={(size / 2) - (4 * strokeWidth)} fill="none" stroke-width={strokeWidth || 2} stroke-miterlimit="10"/>
         </svg>
       </article>
     );
