105ce93875461cd9177e165a06afe7169ec5844f
Ross Court <rosscourt93@gmail.com>
2016-03-21 23:39:02 +0000

    Fix naming error

--- api/actions/system/getGroupz.js
+++ api/actions/system/getGroupz.js
@@ -1,6 +1,6 @@
 import SystemHelper from '../../utils/system';
 
-export default function getGroups() {
+export default function getGroupz() {
   return new Promise((resolve) => {
     resolve(SystemHelper.getGroupz());
   });
